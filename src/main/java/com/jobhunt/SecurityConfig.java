8a2f6ecf35dd52cd61f86ba708c28493837a3569
Ankur Kumar <ankurkumar352019@gmail.com>
2026-01-28 21:32:40 +0530

    Cors allowed origins updated

--- src/main/java/com/jobhunt/SecurityConfig.java
+++ src/main/java/com/jobhunt/SecurityConfig.java
@@ -106,7 +106,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of("http://localhost:3000" , "https://jobhunt-obuw.onrender.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
